1733c59dd363ea70aa3656643010cd327960022d
99hhernandez <30508079+99hhernandez@users.noreply.github.com>
2021-04-13 08:50:54 -0500

    Update NextPCLogic.v

--- Lab07/NextPCLogic.v
+++ Lab07/NextPCLogic.v
@@ -3,21 +3,19 @@ module NextPClogic(NextPC, CurrentPC, SignExtImm64, Branch, ALUZero, Uncondbranc
        input Branch, ALUZero, Uncondbranch; 
        output [63:0] NextPC; 
        
-       reg [63:0] tempImm64;
        reg [63:0] NextPC;
        
        always@(*)
               begin
-                     tempImm64 = SignExtImm64 << 2;                   //shift value over by 2
                      if(Uncondbranch)                                 //for unconditional branches
                      begin
-                            NextPC <= #1 CurrentPC + tempImm64;
+                            NextPC <= #3 CurrentPC + SignExtImm64;
                      end
                      else if(Branch)
                      begin
                             if(ALUZero == 1)
                             begin
-                                   NextPC <= #2 CurrentPC + tempImm64;        //for conditional branches and ALUZero
+                                   NextPC <= #3 CurrentPC + SignExtImm64;        //for conditional branches and ALUZero
                             end
                             else 
                             begin
